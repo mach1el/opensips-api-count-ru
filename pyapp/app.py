ba35a61f7d73b7d27548fcd8f239483f32df5506
mach1el <th3phantom.m1@gmail.com>
2022-09-28 16:06:58 +0700

    Init pyapp

---
+++ pyapp/app.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import psycopg2
+import configparser
+from flask import abort
+from flask import make_response
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+parser = configparser.ConfigParser()
+parser.read('servers')
+
+def count(data):
+  filter = ['200','408','486','487','600']
+  counted = [code[0] for code in data if code[0] in filter]
+  return len(counted)
+
+
+def request_db(requester,extension):
+  try:
+    server = parser['vtfe']
+    conn = psycopg2.connect(
+      host     = server['db_host'],
+      port     = server['db_port'],
+      user     = server['db_user'],
+      password = server['db_password'],
+      database = server['db_name']
+    )
+    cur = conn.cursor()
+    cur.execute("select sip_code from acc where exten='%s'" % extension)
+    data = cur.fetchall()
+    return count(data)
+
+  except: abort(404)
+
+@app.errorhandler(404)
+def not_found(error):
+	return make_response(jsonify({'error' : 'Not found'}), 404)
+
+@app.route('/check',methods=['GET'])
+def checker():
+  args = request.args
+  requester = args.get('from')
+  phone_num = args.get('extension')
+  resp = request_db(requester,phone_num)
+  if resp >= 6:
+    return make_response(jsonify({'status' : 'FAILED'}),504)
+  else:
+    return make_response(jsonify({'status' : 'PASSED'}),200)
+
+if __name__ == '__main__':
+	app.run(host="0.0.0.0",port=2000,debug=True)
